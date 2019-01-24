b1234020a417e8f422753787ee841f60b7f587b1
Dalton Hart <DaltonVaren@gmail.com>
2019-01-24 13:29:20 -0800

    added channel

--- mybot.js
+++ mybot.js
@@ -29,7 +29,7 @@ client.on("ready", () => {
 
 
 client.on("message", (message) => {
-  if(message.type == 'dm') {
+  if(message.channel.type == 'dm') {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
