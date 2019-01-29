2afe52e652d406ab95b08972c4cc069f06691353
Dalton Hart <DaltonVaren@gmail.com>
2019-01-28 19:01:28 -0800

    changed layout of message

--- commands/assign.js
+++ commands/assign.js
@@ -68,8 +68,8 @@ module.exports = {
                     console.log('ERROR', err)
                   }
                   let dueDate = moment(newJob.dueTime).format('MMM Do YY')
-                  let assignedDate = moment(newJob.assignedDate)
-                  let inWorks = assignedDate.fromNow();
+                  let assignedDate = moment(newJob.assignedDate).format('MMM Do YY')
+                  let inWorks = assignedDate.fromNow()
                   
                   
                   db.Job.find({user:assignedUser}).exec((err,jobs)=>{
@@ -79,7 +79,7 @@ module.exports = {
                     const exampleEmbed = new Discord.RichEmbed()
                         .setColor('#724B34')
                         .setTitle(`**TODO:** ${newJob.description}`)
-                        .setDescription(`**Job ID:** ${newJob._id} assigned to ${newJob.user} \n **Complete:** ${newJob.complete}   **Due:** ${newJob.complete} \n **Assigned By:** ${assigner} on ${assignedDate}`)
+                        .setDescription(`**Job ID:** ${newJob._id} assigned to ${newJob.user} \n **Complete:** ${newJob.complete}   **Due:** ${dueDate} \n **Assigned By:** ${assigner} on ${assignedDate}`)
                         // .addField(`COMPLETE:`, `${newJob.complete}`, true)
                         // .addField(`DUE:`,`${dueDate}`, true)
                         .setFooter(`Assigned ${inWorks}`)
