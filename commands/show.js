51649ba9ee8d7c61b2c154586bf9dd939bb90c60
Dalton Hart <DaltonVaren@gmail.com>
2019-01-28 19:36:15 -0800

    adjusted show

--- commands/show.js
+++ commands/show.js
@@ -24,7 +24,7 @@ module.exports = {
                 let now = moment()
                 let assignedDate = moment(found.assignedDate)
 
-                let inWorks = now.to(found.assignedDate)
+                let inWorks = found.assignedDate.to(now)
 
                 let dueDate = moment(found.dueTime).format('MMM Do YY')
                 let assignedDateFormatted = assignedDate.format('MMM Do YY')
