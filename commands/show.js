66ad82441c6ca50f8a01a56ef8ef596b7c2c3eec
Dalton Hart <DaltonVaren@gmail.com>
2019-01-28 19:23:29 -0800

    adjusted show

--- commands/show.js
+++ commands/show.js
@@ -21,9 +21,10 @@ module.exports = {
                 console.log('ERROR', err)
                return message.channel.send(`Invalid id entered.`);
               } else {
+                let now = moment()
                 let dueDate = moment(found.dueTime).format('MMM Do YY')
                 let assignedDate = moment(found.assignedDate)
-                let inWorks = assignedDate.fromNow()
+                let inWorks = now.from(assignedDate)
                 let assignedDateFormatted = assignedDate.format('MMM Do YY')
                 let assignerId = found.assigner.replace(/\D/g,'')
                 let assigner = message.client.users.get(assignerId).username
