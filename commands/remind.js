f2302eb6ce73bfc7099119afd713605f48b2a2a6
Dalton Hart <DaltonVaren@gmail.com>
2019-02-02 16:15:44 -0800

    updated remind to new format

--- commands/remind.js
+++ commands/remind.js
@@ -14,61 +14,80 @@ module.exports = {
     description: 'Sends a Dm of all Jobs that are not completed for the requested user sorted by due date.',
     execute(message, args) {
         let userRequested = `<@${message.author.id}>`
-        db.Job.find({user:userRequested}).exec((err,jobs)=>{
+        db.Job.find({
+            user: userRequested
+        }).exec((err, jobs) => {
 
             let filterJobs = jobs.filter(function (job) {
-                return job.complete == false 
-              });
-            
-            if(filterJobs.length > 0){
-            function compare(a, b) {
-                const dateA = moment(a.dueTime)
-                const dateB = moment(b.dueTime)
-                let comparison = 0
-                if (dateA > dateB) {
-                  comparison = 1;
-                } else if (dateA < dateB) {
-                  comparison = -1;
-                }
-                return comparison;
-              }
-              filterJobs.sort(compare)
+                return job.complete == false
+            });
 
-            filterJobs.forEach((job)=>{
-                let momentToday = moment()
-                let momentJobDate = moment(job.dueTime)
-                let date = momentJobDate.format('MMM Do YYYY')
-
-                if (momentToday > momentJobDate){
-                    let embed = new Discord.RichEmbed()
-                        .setTimestamp(new Date())
-                        .setColor('#C82233')
-                        .setTitle(`Job Due`)
-                        .setDescription(`Job ID: ${job._id}`)
-                        .addField(`TODO:`,`${job.description}`, false)
-                        .addField(`COMPLETE:`, `${job.complete}`, true)
-                        .addField(`DUE:`,`${date}`, true)
-                    message.author.send({embed})
-                } else {
-                    let embed = new Discord.RichEmbed()
-                        .setTimestamp(new Date())
-                        .setColor('#33CD32')
-                        .setTitle(`Job Due`)
-                        .setDescription(`Job ID: ${job._id}`)
-                        .addField(`TODO:`,`${job.description}`, false)
-                        .addField(`COMPLETE:`, `${job.complete}`, true)
-                        .addField(`DUE:`,`${date}`, true)
-                    message.author.send({embed})
+            if (filterJobs.length > 0) {
+                function compare(a, b) {
+                    const dateA = moment(a.dueTime)
+                    const dateB = moment(b.dueTime)
+                    let comparison = 0
+                    if (dateA > dateB) {
+                        comparison = 1;
+                    } else if (dateA < dateB) {
+                        comparison = -1;
+                    }
+                    return comparison;
                 }
-            })
+                filterJobs.sort(compare)
+
+                filterJobs.forEach((found) => {
+                    let momentToday = moment()
+                    let momentJob = moment(found.dueTime)
+                    let date = momentJob.format('MMM Do YYYY')
+
+                    let assignedDate = moment(found.assignedDate)
+                    let inWorks = assignedDate.fromNow()
+                    let dueDate = moment(found.dueTime).format('MMM Do YYYY')
+                    let assignedDateFormatted = assignedDate.format('MMM Do YYYY')
+                    let assignerId = found.assigner.replace(/\D/g, '')
+                    let assigner = message.client.users.get(assignerId).username
+                    let complete;
+                    let completedDate;
+
+                    if (found.complete === false) {
+                        complete = 'Incomplete'
+                        completedDate = 'Not yet Completed'
+                    } else {
+                        complete = 'Complete'
+                        completedDate = moment(found.completedOn).format('MMM Do YYYY')
+                    }
+                    
+                    if (momentToday > momentJob) {
+                        const exampleEmbed = new Discord.RichEmbed()
+                            .setColor('#C82233')
+                            .setTitle(`**TODO:** ${found.description}`)
+                            .setDescription(`**Job ID:** ${found._id} assigned to ${found.user} \n **Due:** ${dueDate} **${complete}** \n **Assigned By:** ${assigner} on ${assignedDateFormatted} \n **Completed By:** ${found.completedBy}  **Completed On:** ${completedDate}`)
+                            .setTimestamp(new Date())
+                            .setFooter(`Assigned ${inWorks}`)
+
+                        message.author.send(exampleEmbed)
+
+                    } else {
+                        const exampleEmbed = new Discord.RichEmbed()
+                            .setColor('#33CD32')
+                            .setTitle(`**TODO:** ${found.description}`)
+                            .setDescription(`**Job ID:** ${found._id} assigned to ${found.user} \n **Due:** ${dueDate} **${complete}** \n **Assigned By:** ${assigner} on ${assignedDateFormatted} \n **Completed By:** ${found.completedBy}  **Completed On:** ${completedDate}`)
+                            .setTimestamp(new Date())
+                            .setFooter(`Assigned ${inWorks}`)
+
+                        message.author.send(exampleEmbed)
+
+                    }
+                })
 
             } else {
                 message.author.send(`You have completed all jobs. Great Job! :)`)
             }
 
-            if (message.channel.type === 'dm') return; 
-                message.reply('I\'ve sent you a DM with your jobs!');
-            })
-            
+            if (message.channel.type === 'dm') return;
+            message.reply('I\'ve sent you a DM with your jobs!');
+        })
+
     },
 };
